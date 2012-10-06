ec12874cc1e57fd6e3511fe98082cb0224d9d80d
居正中 <AlanJui.1960@gmail.com>
2012-10-06 12:28:20 +0800

    Initial Commit

---
+++ myWebView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  myWebView
+//
+//  Created by 居正中 on 12/10/6.
+//  Copyright (c) 2012年 CCC Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ myWebView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  myWebView
+//
+//  Created by 居正中 on 12/10/6.
+//  Copyright (c) 2012年 CCC Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
